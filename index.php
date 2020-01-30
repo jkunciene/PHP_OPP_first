68f443919f4e5a6032d65befeb25351b0f96738a
jkunciene <j.kunciene@gmail.com>
2020-01-30 12:19:34 +0200

    uzduoties pradzia iki paveldejimo

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ class 	TransportoPriemone{
       private $marke;
       private $kaina;
       private $svoris;
+      private $variklis;
       private $variklioGalingumas;
       private $maksimalusGreitis;
       private $aprasymas;
@@ -21,10 +22,27 @@ class 	TransportoPriemone{
     public function addSvoris($svoris){
         $this->svoris = $svoris;
     }
+    public function addVariklis($variklis){
+        $this->variklis = $variklis;
+    }
     public function addVariklioGalingumas($variklioGalingumas){
-        $this->variklioGalingumas = $variklioGalingumas;
+        if ($this->variklis == true){
+            $this->variklioGalingumas = $variklioGalingumas;
+        } else {
+            echo "priemone neturi variklio";
+        }
 
     }
+    public function addMaksimalusGreitis($maksimalusGreitis){
+        $this->maksimalusGreitis = $maksimalusGreitis;
+    }
+    public function addAprasymas($aprasymas){
+        $this->aprasymas = $aprasymas;
+    }
+    public function addTalpinaZmoniu($talpinaZmoniu){
+        $this->talpinaZmoniu = $talpinaZmoniu;
+    }
+
     //geteriai
     public function show(){ //geteriai grazina reikasmes
         $data[] = $this->modelis;
@@ -40,7 +58,15 @@ class 	TransportoPriemone{
     public function showVariklioGalingumas(){
         return $this->variklioGalingumas;
     }
-
+    public function showMaksimalusGreitis(){
+            return $this->maksimalusGreitis;
+    }
+    public function showAprasymas(){
+        return $this->aprasymas;
+    }
+    public function showTalpinaZmoniu(){
+        return $this->talpinaZmoniu;
+    }
 }
 
 
@@ -48,6 +74,10 @@ $automobilis = new TransportoPriemone('penktas', 'BMW');
 $automobilis->addKaina(5000);
 $automobilis->addSvoris(3500);
 $automobilis->addVariklioGalingumas(3);
+$automobilis->addMaksimalusGreitis(220);
+$automobilis->addAprasymas('Greitas fainas masiniukas, tik sunkiai suvairuojamas');
+$automobilis->addTalpinaZmoniu(4);
+
 ?>
 <ul>
     <?php foreach ($automobilis->show() as $value): ?>
@@ -57,4 +87,7 @@ $automobilis->addVariklioGalingumas(3);
 <li><?=$automobilis->showKaina();?></li>
     <li><?=$automobilis->showSvoris(); ?></li>
     <li><?=$automobilis->showVariklioGalingumas(); ?></li>
+    <li><?=$automobilis->showMaksimalusGreitis(); ?></li>
+    <li><?=$automobilis->showAprasymas(); ?></li>
+    <li><?=$automobilis->showTalpinaZmoniu(); ?></li>
 </ul>
