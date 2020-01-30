df8364362ba616371c1257579eecbeeeac793a6d
jkunciene <j.kunciene@gmail.com>
2020-01-30 12:48:05 +0200

    test composer

--- add/User.php
+++ add/User.php
@@ -5,8 +5,18 @@
  * Date: 2020-01-30
  * Time: 12:24
  */
-
+namespace OOP;
 class User
 {
+    protected $name;
+    protected $email;
+    protected $phone;
+    protected $comment;
 
+    public function __construct($name, $email, $phone)
+    {
+        $this->name = $name;
+        $this->name = $email;
+        $this->name = $phone;
+    }
 }
--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php
+require "vendor/autoload.php";
+use OOP\User;
+
+$user1 = new User("Jonukas", "jons@gmail.com", +37060375979);
+
+
+
